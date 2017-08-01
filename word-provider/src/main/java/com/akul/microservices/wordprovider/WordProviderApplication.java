dea396935892d1b6072f8a53a6ba959d8f442f03
akulgoyal <akul@theinnovationinc.co>
2017-08-01 12:50:37 +0530

    Staging micro-services

---
+++ word-provider/src/main/java/com/akul/microservices/wordprovider/WordProviderApplication.java
@@ -0,0 +1,14 @@
+package com.akul.microservices.wordprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class WordProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordProviderApplication.class, args);
+	}
+}
---
+++ word-provider/src/test/java/com/akul/microservices/wordprovider/WordProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.akul.microservices.wordprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WordProviderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
